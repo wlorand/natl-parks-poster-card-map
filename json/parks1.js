b23ec2eec05957fe9e5bcff050629f4d09896f45
Bill Lorand <bill@milemarker3.org>
2016-04-17 14:58:55 -0700

    initial commit

---
+++ json/parks1.js
@@ -0,0 +1,11 @@
+{
+	"parks" : [
+  	{
+      "name" : "Hawaii Volcanoes National Park",
+      "estDate" : "1916",
+      "lat" : 19.3833352,
+      "lon" : -155.2000045,
+      "shortDesc" : "Active Volcanoes and Lava Beds"
+    }
+	]
+}
